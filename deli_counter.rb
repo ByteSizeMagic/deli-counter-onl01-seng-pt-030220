5a6a8fb682ed6b659f9796de9a50c4973190ff3a
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-09 19:47:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+# Beginning of day the line is empty
+katz_deli = []
+
+# shows current place in line
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  elsif katz_deli > 0
+    puts "The line is currently: "
+  end
+end
+
+
+# new customer takes number
+def take_a_number(katz_deli, customer_name)
+# need to add name to end of line
+
+end
+
+
+# prints next person in line and removes from front of line
+def now_serving
+  if
+    puts "There is nobody waiting to be served!"
+end
+end
